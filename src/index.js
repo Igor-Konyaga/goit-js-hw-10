c160df9f89c4c9fa1183f5ae0f115f9c672702de
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-21 22:06:25 +0300

    change creatMarkup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed,} from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const selectEl = document.querySelector('.breed-select');
 const catInfoEl = document.querySelector('.cat-info');
@@ -22,13 +22,11 @@ function onBreedCat(e) {
   fetchCatByBreed(optionValue)
     .then(cat => creatMarkup(cat.data))
     .catch(err => console.error(err));
-
 }
 
 function creatMarkup(arr) {
-  const markup = arr.map(({url}) => {
-    return `<img src=${url} alt="cat" width="500" haight="400">`;
-  });
+  const markup = `<img src=${arr[0].url} alt="cat" width="500" haight="400">
+	 <h2>${arr[0].breeds[0].name}</h2> <p>${arr[0].breeds[0].description}</p> <p>Temperament: ${arr[0].breeds[0].temperament}</p>`;
+
   catInfoEl.innerHTML = markup;
 }
-
