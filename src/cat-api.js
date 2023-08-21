87acd1dcb1f044b04b54d8aefcb498c8c98e2b47
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-08-21 21:13:54 +0300

    change

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 
 const API_KEY =
-'live_ ZRswS3nkdpDObPAFELcNEy6P3yBL78 aSLcLEpcvRYiOrKQPwmC2UCVnyeUFT nZbv';
+  'live_ZRswS3nkdpDObPAFELcNEy6P3yBL78aSLcLEpcvRYiOrKQPwmC2UCVnyeUFTnZbv';
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 
 axios.defaults.headers.common['x-api-key'] = API_KEY;
 
- export function fetchBreeds() {
- 
-  return axios(`${BASE_URL}breeds`)
+export function fetchBreeds() {
+  return axios(`${BASE_URL}breeds`);
 }
 
-
- export function fetchCatByBreed(breedId){
-
-	return axios(`${BASE_URL}images/search?breed_ids=${breedId}`,)
- }
-
+export function fetchCatByBreed(breedId) {
+  return axios(`${BASE_URL}images/search?breed_ids=${breedId}`);
+}
